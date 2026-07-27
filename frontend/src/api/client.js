dad5e82e07a0b1768d903f0d4b4050660f600f47
jason <jasonzheng0814@gmail.com>
2026-07-27 14:49:17 -0400

    make frontend and api same-origin

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: import.meta.env.VITE_API_URL ?? '/api',
 })
 
 client.interceptors.request.use((config) => {
